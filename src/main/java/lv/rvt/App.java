759681c8bd91bc5496772716febbe668bd7b8755
23DP4MAuza <166509201+23DP4MAuza@users.noreply.github.com>
2024-09-13 11:26:35 +0000

    Uzdevums 0

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -1,9 +1,27 @@
 package lv.rvt;
 
+
+import java.util.*;
+
+
+
 public class App 
 {
     public static void main( String[] args )
     {
-        // Make your Hello World programm!
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Ievadiet vārdu: ");
+        String name = scanner.nextLine();
+
+        System.out.println("Ievadiet uzvārdu: ");
+        String lastname = scanner.nextLine();
+
+        System.out.println("Ievadiet grupu: ");
+        String group = scanner.nextLine();
+
+
+
+        System.out.println("Students: "+ name + " " + lastname + ", Grupa: " + group);
     }
 }
