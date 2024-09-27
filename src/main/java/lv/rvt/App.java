47464afa59ecccc829d6bd5a0fa9d0474449576f
23DP4MAuza <166509201+23DP4MAuza@users.noreply.github.com>
2024-09-27 11:22:11 +0000

    Gift tax+

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -48,27 +48,51 @@ public class App {
         //     System.out.println("Something else!");
         // }
 
+        //         Scanner scanner = new Scanner(System.in);
+
+        //         System.out.println("Give points [0-100]: ");
+        //         int points = Integer.valueOf(scanner.nextLine());
+
+        //         if (points < 0) {
+        //             System.out.println("impossible!");
+        //         } else if (points > 100) {
+        //             System.out.println("incredible!");
+        //         } else if (points <= 49) {
+        //             System.out.println("failed");
+        //         } else if (points <= 59) {
+        //             System.out.println("1");
+        //         } else if (points <= 69) {
+        //             System.out.println("2");
+        //         } else if (points <= 79) {
+        //             System.out.println("3");
+        //         } else if (points <= 89) {
+        //             System.out.println("4");
+        //         } else {
+        //             System.out.println("5");
+        //         }
+
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Give points [0-100]: ");
-        int points = Integer.valueOf(scanner.nextLine());
+        System.out.print("Value of the gift? ");
+        double giftValue = scanner.nextDouble();
 
-        if (points < 0) {
-            System.out.println("impossible!");
-        } else if (points > 100) {
-            System.out.println("incredible!");
-        } else if (points <= 49) {
-            System.out.println("failed");
-        } else if (points <= 59) {
-            System.out.println("1");
-        } else if (points <= 69) {
-            System.out.println("2");
-        } else if (points <= 79) {
-            System.out.println("3");
-        } else if (points <= 89) {
-            System.out.println("4");
+        if (giftValue <= 5000) {
+            System.out.println("No tax!");
+        } else if (giftValue <= 25000) {
+            double tax = 100 + (giftValue - 5000) * 0.08;
+            System.out.printf("Tax: ", tax);
+        } else if (giftValue <= 55000) {
+            double tax = 1700 + (giftValue - 25000) * 0.10;
+            System.out.printf("Tax: ", tax);
+        } else if (giftValue <= 200000) {
+            double tax = 4700 + (giftValue - 55000) * 0.12;
+            System.out.printf("Tax: ", tax);
+        } else if (giftValue <= 1000000) {
+            double tax = 22100 + (giftValue - 200000) * 0.15;
+            System.out.printf("Tax: ", tax);
         } else {
-            System.out.println("5");
+            double tax = 142100 + (giftValue - 1000000) * 0.17;
+            System.out.printf("Tax: ", tax);
         }
     }
 }
