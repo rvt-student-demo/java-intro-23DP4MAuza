8249870aa35250e828c15c02064f1d801af44c7e
23DP4MAuza <166509201+23DP4MAuza@users.noreply.github.com>
2024-10-31 13:15:42 +0000

    Same sum

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -8,13 +8,21 @@ public class App
   public static void main ( String[] args )
   {
     int[] valA   = { 13, -22,  82,  17}; 
-    int[] valB   = {-12,  24, -79, -13};
-    int[] sum    = {  0,   0,   0,   0};
-    for (int i = 0; i < valA.length; i++) {
-      sum[i] = valA[i] + valB[i];
-    }
+    int[] valB   = {  12,   47,   -57,   8};
+    
+    // Put values into valB so that the sum of the values
+    // in corresponding cells of valA and valB is 25.
 
-    System.out.println( "sum: " 
-        + sum[0] + " " + sum[1] + " " + sum[2] + " " + sum[3] );
+ 
+ 
+    System.out.println( "valA: " 
+        + valA[0] + " " + valA[1] + " " + valA[2] + " " + valA[3] );
+ 
+    System.out.println( "valB: " 
+        + valB[0] + " " + valB[1] + " " + valB[2] + " " + valB[3] );
+
+    System.out.println( "sum:  " 
+        + (valA[0]+valB[0]) + " " + (valA[1]+valB[1]) + " " 
+        + (valA[2]+valB[2]) + " " + (valA[3]+valB[3]) );
    }
 }
