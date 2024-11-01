cdb26ecda611c373bfcedeb55e9c6c87e8160cb8
23DP4MAuza <166509201+23DP4MAuza@users.noreply.github.com>
2024-11-01 12:34:17 +0000

    Personal details

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -1,24 +1,45 @@
 package lv.rvt;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
 
-// import java.util.*;
+public class App {
 
-public class App
-{
-  public static void main ( String[] args )
-  {
-    int[] val = {0, 1, 2, 3}; 
-    int temp;
- 
-    System.out.println( "Original Array: " 
-        + val[0] + " " + val[1] + " " + val[2] + " " + val[3] );
- 
-    // reverse the order of the numbers in the array
- 
- 
-    
- 
-    System.out.println( "Reversed Array: " 
-        + val[3] + " " + val[2] + " " + val[1] + " " + val[0] );
-   }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        HashMap<String, Integer> nameAge = new HashMap<>();
+        while (true) {
+            String sentence = scanner.nextLine();
+            if (sentence.isEmpty()) {
+                break;
+            }
+            String[] arraySentence = sentence.split(",");
+            int age = Integer.valueOf(arraySentence[1]);
+            nameAge.put(arraySentence[0], age);
+        }
+        
+        int longestLength = Integer.MIN_VALUE;
+        String longestName = "";
+        for (Map.Entry<String, Integer> entry : nameAge.entrySet()) { 
+            if (entry.getKey().length() > longestLength) {
+                longestLength = entry.getKey().length();
+                longestName = entry.getKey();
+            }
+        }  
+        
+        System.out.println("Garakais vārds: " + longestName);
+        
+        int sum = 0;
+        int count = 0;
+        for (Map.Entry<String, Integer> entry : nameAge.entrySet()) {  
+            sum += entry.getValue();
+            count++;
+        }  
+        
+        double average = (double) sum / count;
+        System.out.println("Vidējais starp visiem dzimšanas gadiem: " + average);
+
+    }
 }
