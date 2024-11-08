1fa8c33d5d447153af77927178029f6fe622ea3b
23DP4MAuza <166509201+23DP4MAuza@users.noreply.github.com>
2024-11-08 11:43:56 +0000

    Product

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -1,45 +1,8 @@
 package lv.rvt;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Scanner;
-
 public class App {
-
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        HashMap<String, Integer> nameAge = new HashMap<>();
-        while (true) {
-            String sentence = scanner.nextLine();
-            if (sentence.isEmpty()) {
-                break;
-            }
-            String[] arraySentence = sentence.split(",");
-            int age = Integer.valueOf(arraySentence[1]);
-            nameAge.put(arraySentence[0], age);
-        }
-        
-        int longestLength = Integer.MIN_VALUE;
-        String longestName = "";
-        for (Map.Entry<String, Integer> entry : nameAge.entrySet()) { 
-            if (entry.getKey().length() > longestLength) {
-                longestLength = entry.getKey().length();
-                longestName = entry.getKey();
-            }
-        }  
-        
-        System.out.println("Garakais vārds: " + longestName);
-        
-        int sum = 0;
-        int count = 0;
-        for (Map.Entry<String, Integer> entry : nameAge.entrySet()) {  
-            sum += entry.getValue();
-            count++;
-        }  
-        
-        double average = (double) sum / count;
-        System.out.println("Vidējais starp visiem dzimšanas gadiem: " + average);
-
+    public static void main(String[] argsg){
+        Product banana = new Product("Banana",1.1,13);
+        banana.printProduct();
     }
 }
---
+++ src/main/java/lv/rvt/Person.java
@@ -0,0 +1,11 @@
+package lv.rvt;
+
+public class Person {
+    private String name;
+    private int age;
+
+    public Person(String initialName) {
+        this.age = 0;
+        this.name = initialName;
+    }
+}
