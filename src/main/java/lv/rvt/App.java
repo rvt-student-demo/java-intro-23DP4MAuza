b8655864365e0a889972ac251cf453ba21b1bcb6
23DP4MAuza <166509201+23DP4MAuza@users.noreply.github.com>
2024-11-22 11:33:50 +0000

    One Minute

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -1,15 +1,37 @@
 package lv.rvt;
 
 public class App {
+    private int seconds;
+    private int hundredths;
+
+    public App() {
+        this.seconds = 0;
+        this.hundredths = 0;
+    }
+
+    public String toString() {
+        return String.format("%02d:%02d", seconds, hundredths);
+    }
+
+    public void advance() {
+        hundredths++;
+        
+        if (hundredths == 100) {
+            hundredths = 0;
+            seconds++;
+        }
+
+        if (seconds == 60) {
+            seconds = 0;
+        }
+    }
+
     public static void main(String[] args) {
-        DecreasingCounter counter = new DecreasingCounter(100);
-
-        counter.printValue();
-
-        counter.reset();
-        counter.printValue();
-
-        counter.decrement();
-        counter.printValue();
+        App timer = new App();
+        
+        for (int i = 0; i < 150; i++) {
+            System.out.println(timer);
+            timer.advance();
+        }
     }
 }
