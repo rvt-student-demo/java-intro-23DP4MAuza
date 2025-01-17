45c259927e9a5057d39dfdc5d171014193dde614
Markuss Auzāns DP2-4 <166509201+23DP4MAuza@users.noreply.github.com>
2025-01-17 10:35:40 +0000

    Chapter 50 - Exercise 3 Box constructor and Access Methods

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -6,7 +6,8 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) {
         Box box = new Box(2.5, 5.0, 6.0);
-
         System.out.println("Area: " + box.area() + " volume: " + box.volume());
+    
+        
     }
 }
--- src/main/java/lv/rvt/Box.java
+++ src/main/java/lv/rvt/Box.java
@@ -2,32 +2,38 @@ package lv.rvt;
 
 public class Box {
     private double length;
-    private double width;
     private double height;
+    private double width;
 
-    public Box(double length, double width, double height) {
+    public Box(double length, double height, double width) {
         this.length = length;
-        this.width = width;
         this.height = height;
+        this.width = width;
+    }
+
+    public Box(Box oldBox) {
+        this.length = oldBox.length();
+        this.height = oldBox.height();
+        this.width = oldBox.width();
+    }
+
+    public double length() {
+        return this.length;
+    }
+
+    public double height() {
+        return this.height;
+    }
+
+    public double width() {
+        return this.width;
     }
 
     public double area() {
-        return 2 * faceArea() + 2 * topArea() + 2 * sideArea();
-    }
-
-    private double faceArea() {
-        return length * height;
-    }
-
-    private double topArea() {
-        return length * width;
-    }
-
-    private double sideArea() {
-        return width * height;
+        return 2 * (length * height + height * width + length * width);
     }
 
     public double volume() {
-        return length * width * height;
+        return length * height * width;
     }
 }
