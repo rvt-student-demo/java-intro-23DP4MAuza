8db4d98cce9a4ce3a966ab19c694ed7846effaee
Markuss Auzāns DP2-4 <166509201+23DP4MAuza@users.noreply.github.com>
2025-01-31 11:06:32 +0000

    Part 9: Part 3 Students toString

--- src/main/java/lv/rvt/Student.java
+++ src/main/java/lv/rvt/Student.java
@@ -1,17 +1,23 @@
 package lv.rvt;
+
 public class Student extends Person {
-    private int credits;
+    private int studyCredits;
 
-    public Student(String name, String address) {
+    public Student(String name, String address, int studyCredits) {
         super(name, address);
-        this.credits = 0;
+        this.studyCredits = studyCredits;
     }
 
-    public int credits() {
-        return credits;
+    @Override
+    public String toString() {
+        return super.toString() + "\nStudy credits " + studyCredits;
     }
 
-    public void study() {
-        credits++;
+    public static void main(String[] args) {
+        Student ollie = new Student("Ollie", "6381 Hollywood Blvd. Los Angeles 90028", 0);
+        System.out.println(ollie.toString());
+
+        ollie = new Student("Ollie", "6381 Hollywood Blvd. Los Angeles 90028", 1);
+        System.out.println(ollie.toString());
     }
 }
